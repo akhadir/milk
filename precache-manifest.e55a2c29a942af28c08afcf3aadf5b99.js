a2cb67137da536b1e3b0700c4052cf5deab42ca0
akhadir <khadir.k@gmail.com>
2019-08-20 10:30:07 +0530

    Testing

---
+++ precache-manifest.e55a2c29a942af28c08afcf3aadf5b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2df32dbaa1125346b5987b64c238f08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bac8d9a9fe8bf3d40a4e",
+    "url": "/static/css/2.2a890d7b.chunk.css"
+  },
+  {
+    "revision": "1693b282dcd00e0e8524",
+    "url": "/static/css/main.043a5cc6.chunk.css"
+  },
+  {
+    "revision": "bac8d9a9fe8bf3d40a4e",
+    "url": "/static/js/2.f5a345af.chunk.js"
+  },
+  {
+    "revision": "1693b282dcd00e0e8524",
+    "url": "/static/js/main.9401584d.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  }
+]);
